965ecfe8eae8e39d4d751de6b7608a56b0a03990
Keiko <Keiko@kanazawayoshiko-no-MacBook-Air.local>
2015-09-22 15:31:42 +0900

    First_step

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def help
+  end
+
+  def new
+  end
+
+  def character
+  end
+
+  def map
+  end
+
+  def result_DL
+  end
+
+  def bss
+  end
+
+  def link
+  end
+end
